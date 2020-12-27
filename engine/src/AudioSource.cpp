983449d6e14adc5a4d639475bd9b2d19623f23f1
Nils <nilsland31@gmail.com>
2020-12-27 20:21:53 +0100

    Fixed buffer destruction problem

--- engine/src/AudioSource.cpp
+++ engine/src/AudioSource.cpp
@@ -12,12 +12,14 @@ namespace engine
         alSourcef(m_source, AL_GAIN, m_gain);
         alSource3f(m_source, AL_POSITION, m_position[0], m_position[1], m_position[2]);
         alSource3f(m_source, AL_VELOCITY, m_velocity[0], m_velocity[1], m_velocity[2]);
-        alSourcei(m_source, AL_LOOPING, m_loopAudio);
+        //alSourcei(m_source, AL_LOOPING, m_loopAudio);
         alSourcei(m_source, AL_BUFFER, m_buffer);
     }
 
     AudioSource::~AudioSource()
     {
+        alSourceStop(m_source);
+        alSourcei(m_source, AL_BUFFER, 0);
         std::cout << "[AudioSource] Delete the following audio source: " << m_source << std::endl;
         alDeleteSources(1, &m_source);
     }

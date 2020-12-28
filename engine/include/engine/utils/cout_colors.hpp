83486e329740abaaa8680731076414fac6be96ac
Nils <nilsland31@gmail.com>
2020-12-28 01:38:05 +0100

    Added colors in cout_colors.hpp

--- engine/include/engine/utils/cout_colors.hpp
+++ engine/include/engine/utils/cout_colors.hpp
@@ -12,5 +12,7 @@ namespace engine
 #define COLOR_RED "\033[31m"
 #define COLOR_GREEN "\033[32m"
 #define COLOR_YELLOW "\033[33m"
+#define COLOR_BLUE "\033[34m"
+#define COLOR_CYAN "\033[96m"
 
 } // namespace engine

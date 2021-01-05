cadf92c1b44d226bf97a87f738ea3aad3e7dd376
Nils <nilsland31@gmail.com>
2021-01-05 18:15:37 +0100

    Small changes to music

--- engine/include/engine/OpenALManager.hpp
+++ engine/include/engine/OpenALManager.hpp
@@ -30,6 +30,9 @@ namespace engine
 
         std::unique_ptr<AudioSource> m_mainSource;
         std::unique_ptr<AudioBuffer> m_mainSourceBuffer;
+
+        std::unique_ptr<AudioSource> m_boatSource;
+        std::unique_ptr<AudioBuffer> m_boatBuffer;
         ALint m_isPlaying=AL_STOPPED;
 
 
--- engine/src/OpenALManager.cpp
+++ engine/src/OpenALManager.cpp
@@ -7,7 +7,7 @@
 namespace engine
 {
 
-    OpenALManager::OpenALManager():m_audioDevice(new AudioDevice()),m_audioListener(new AudioListener()), m_mainSource(new AudioSource()), m_mainSourceBuffer(new AudioBuffer())
+    OpenALManager::OpenALManager():m_audioDevice(new AudioDevice()),m_audioListener(new AudioListener()), m_mainSource(new AudioSource()), m_mainSourceBuffer(new AudioBuffer()), m_boatSource(new AudioSource()), m_boatBuffer(new AudioBuffer())
     {
         initialize();
     }
@@ -20,12 +20,17 @@ namespace engine
       m_mainSource->setLooping(1);
       m_mainSource->setBuffer(m_mainSourceBuffer.get()->m_audioEffectBuffers[0]);
 
+      m_boatBuffer->addAudioEffect("application/res/sounds/boat.wav");
+      m_boatSource->setLooping(1);
+      m_boatSource->setBuffer(m_boatBuffer.get()->m_audioEffectBuffers[0]);
+
 
       alGetSourcei(m_mainSource->m_source, AL_SOURCE_STATE, &m_isPlaying);
 
       if (m_isPlaying != AL_PLAYING || alGetError() != AL_NO_ERROR)
       {
           m_mainSource->play();
+          m_boatSource->play();
       }
       return 0;
     }
@@ -36,15 +41,18 @@ namespace engine
       glm::vec3 cameraPosition = GLApplication::getInstance().getCamera()->getPosition();
       m_audioListener->setPosition(cameraPosition);
       m_mainSource->setPosition(cameraPosition);
+      m_boatSource->setPosition(cameraPosition);
     }
 
     void OpenALManager::pause(){
       alGetSourcei(m_mainSource->m_source, AL_SOURCE_STATE, &m_isPlaying);
       if(m_isPlaying == AL_PLAYING){
         m_mainSource->pause();
+        m_boatSource->pause();
       }
       else{
         m_mainSource->play();
+        m_boatSource->play();
       }
     }
 
@@ -53,6 +61,7 @@ namespace engine
     void OpenALManager::destroy()
     {
       m_mainSource->stop();
+      m_boatSource->stop();
       m_isPlaying=AL_STOPPED;
     }
 
